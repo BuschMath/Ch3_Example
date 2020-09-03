3cf27ce7ae9f7ba53ad8db1f4126c91bf917171b
BuschMath <47864618+BuschMath@users.noreply.github.com>
2020-09-03 09:51:04 -0500

    String manip

--- Ch3_Example/Main.cpp
+++ Ch3_Example/Main.cpp
@@ -1,30 +1,20 @@
 #include <iostream>
-#include <iomanip>
-
-int func(int a, int b);		// prototype
+#include <string>
 
 int main()
 {
-	std::cout << func(2, 7) << std::endl;		// call
+	std::string firstName = "Tony";
+	std::string lastName = "Stark";
 
-	std::cout << std::setw(7) << "1234" << std::endl;
-	std::cout << std::setw(6) << "1234" << std::endl;
-	std::cout << std::setw(5) << "1234" << std::endl;
-	std::cout << std::setw(4) << "1234" << std::endl;
-	std::cout << std::setw(3) << "1234" << std::endl;
+	std::string fullName = firstName + " " + lastName;
 
-	float x = 310.0, y = 4.827;
-	std::cout << std::fixed << std::setw(10) << std::setprecision(5) << x << std::endl;
-	std::cout << std::setw(6) << std::setprecision(2) << y << std::endl;
+	std::cout << firstName << " has " << firstName.length() << " characters in it.\n";
+	std::cout << fullName << " has " << fullName.length() << " characters in it.\n";
 
-	std::cout << std::showpoint << std::setprecision(2) << "$" << float(100);
+	// remember 0 indexed, finds the first occurance
+	std::cout << "The space between first name and last name is character: " << fullName.find(" ") << std::endl; 
 
+	std::cout << "This is a substring: " << fullName.substr(3, 5) << std::endl;
 
 	return 0;
-}
-
-// definition
-int func(int a, int b)
-{
-	return a + b;
 }
