042a4da5acdf4a5933a093be442cfd8484ea6022
BuschMath <47864618+BuschMath@users.noreply.github.com>
2020-09-03 08:45:46 -0500

    Formatting Output

--- Ch3_Example/Main.cpp
+++ Ch3_Example/Main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <iomanip>
 
 int func(int a, int b);		// prototype
 
 int main()
 {
-	std::cout << func(2, 7);		// call
+	std::cout << func(2, 7) << std::endl;		// call
+
+	std::cout << std::setw(7) << "1234" << std::endl;
+	std::cout << std::setw(6) << "1234" << std::endl;
+	std::cout << std::setw(5) << "1234" << std::endl;
+	std::cout << std::setw(4) << "1234" << std::endl;
+	std::cout << std::setw(3) << "1234" << std::endl;
+
+	float x = 310.0, y = 4.827;
+	std::cout << std::fixed << std::setw(10) << std::setprecision(5) << x << std::endl;
+	std::cout << std::setw(6) << std::setprecision(2) << y << std::endl;
+
+	std::cout << std::showpoint << std::setprecision(2) << "$" << float(100);
+
 
 	return 0;
 }
